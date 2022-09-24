992987a78c3aff114936368e577371c000f3e2bc
Paulo Henrique <phferreirasouza106@gmail.com>
2022-09-24 20:37:05 -0300

    fix(scripts): add support for asdf and place neovim globally

--- scripts/os/linux.sh
+++ scripts/os/linux.sh
@@ -1,17 +1,21 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Install Neovim
-cd ~/Workspace && mdkir -p tmp && cd tmp
-curl -lo https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+mkdir -p ~/tmp && cd ~/tmp
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 ./squashfs-root/apprun --version
-
-# Install PlugInstall
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 # Install Asdf
-sudo apt-get install -y dirmngr gpg curl gawk
-git clone https://github.com/asdf-vm/asdf.gik ~/.asdf --branch v0.10.2 
+mkdir -p ~/.asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2 
 echo ". $HOME/.asdf/asdf.sh" >> ~/.zshrc
+
+# Asdf-node dependencies
+sudo apt-get install -y dirmngr gpg curl gawk
+
+# Asdf-go dependencies
+sudo apt install -y coreutils
