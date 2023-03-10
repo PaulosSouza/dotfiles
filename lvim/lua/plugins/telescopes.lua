689073e673fc99915fe45c73ac0ae5a9e38632be
Paulo Henrique <phferreirasouza106@gmail.com>
2023-03-10 20:07:07 -0300

    feat(lvim): new configs for telescope

--- lvim/lua/plugins/telescopes.lua
+++ lvim/lua/plugins/telescopes.lua
@@ -10,3 +10,15 @@ lvim.builtin.which_key.mappings["f"] = {
   o = { "<cmd>lua require('telescope.builtin').oldfiles()<cr>", "Old files" },
   h = { "<cmd>lua require('telescope.builtin').help_tags()<cr>", "Help tags" },
 }
+
+lvim.builtin.telescope.defaults.layout_config.width = 0.85
+lvim.builtin.telescope.defaults.layout_config.preview_cutoff = 80
+
+lvim.builtin.telescope.pickers.find_files = {}
+lvim.builtin.telescope.pickers.live_grep = {
+  only_sort_text = true,
+}
+lvim.builtin.telescope.pickers.grep_string = {
+  only_sort_text = true,
+}
+lvim.builtin.telescope.pickers.git_files = {}
