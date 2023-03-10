5517065a920eb754ce9df23acb7998c7e40fa76b
Paulo Henrique <phferreirasouza106@gmail.com>
2023-03-10 20:05:41 -0300

    feat(lvim): add new keybinds

--- lvim/lua/plugins/keybinds.lua
+++ lvim/lua/plugins/keybinds.lua
@@ -2,19 +2,18 @@
 lvim.leader = "space"
 
 -- Normal Mode
-lvim.keys.normal_mode["<A-j>"] = ":w<cr>"
 lvim.keys.normal_mode["<leader>c"] = "<CMD>edit " .. require("lvim.config"):get_user_config_path() .. " <CR>"
-lvim.keys.normal_mode["<S-l>"] = "$"
-lvim.keys.normal_mode["<S-h>"] = "^"
-lvim.keys.normal_mode["<C-0>"] = ":ToggleTerm<cr>"
 
--- Visual Mode
-lvim.keys.visual_mode["<S-l>"] = "$"
-lvim.keys.visual_mode["<S-h>"] = "^"
+-- End of lines
+lvim.keys.normal_mode["H"] = "^"
+lvim.keys.normal_mode["L"] = "$"
+lvim.keys.visual_mode["H"] = "^"
+lvim.keys.visual_mode["L"] = "$"
 
 -- Which key
 lvim.builtin.which_key.mappings["v"] = {
   name = "Splits",
   v = { ":vsplit<cr>", "Vertical" },
   h = { ":split<cr>", "Horizontal" },
+  o = { ":SymbolsOutline<cr>", "Open Symbols Outline" },
 }
