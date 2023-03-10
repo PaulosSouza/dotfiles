f3cad2aaf7cec967336de20f51b7e052568c6d57
Paulo Henrique <phferreirasouza106@gmail.com>
2023-03-10 20:06:05 -0300

    feat(lvim): new statusline approach

--- lvim/lua/plugins/statusline.lua
+++ lvim/lua/plugins/statusline.lua
@@ -5,9 +5,8 @@ lvim.builtin.lualine.options.theme = "catppuccin"
 lvim.builtin.lualine.sections = {
   lualine_a = { components.mode },
   lualine_b = { components.filename },
-  lualine_c = { components.filetype, components.branch },
-
-  lualine_x = { components.diff, components.diagnostics, components.lsp },
+  lualine_c = { components.branch, components.diff, components.diagnostics },
+  lualine_x = { components.filetype, components.lsp },
   lualine_y = { components.treesitter },
   lualine_z = {}
 }
