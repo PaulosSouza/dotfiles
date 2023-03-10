20a247c747fce59966772fb9ca74f6fa7ced0cf1
Paulo Henrique <phferreirasouza106@gmail.com>
2023-03-10 20:05:24 -0300

    feat(lvim): add eslint as a formatters

--- lvim/lua/plugins/formaters.lua
+++ lvim/lua/plugins/formaters.lua
@@ -2,12 +2,13 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { name = "prettierd", filetypes = { "javascript", "typescript", "typescriptreact", "javascriptreact", "html", "css" } },
   { name = "gofumpt" },
+  { name = "eslint_d" },
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-  { name = "eslint_d" },
   { name = "golangci_lint" },
+  { name = "eslint_d" },
 }
 
 local code_actions = require "lvim.lsp.null-ls.code_actions"
