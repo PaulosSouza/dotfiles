312811639b2ff6644f7d4d671ff6461abd75742f
Paulo Henrique <phferreirasouza106@gmail.com>
2023-03-10 20:05:54 -0300

    feat(lvim): add new lsps

--- lvim/lua/plugins/lsp.lua
+++ lvim/lua/plugins/lsp.lua
@@ -1,1 +1,5 @@
-require("lvim.lsp.manager").setup("ltex")
+local opts = {}
+
+require("lvim.lsp.manager").setup("cssmodules_ls", opts)
+require("lvim.lsp.manager").setup("emmet_ls", opts)
+require("lvim.lsp.manager").setup("editorconfig_checker", opts)
